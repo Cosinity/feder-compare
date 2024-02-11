3f6168d9c7ffd50ad34b6a14854631352b7fa6b3
Cosinity <accostenoble@gmail.com>
2024-02-11 13:33:38 -0500

    First working commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import data from '../assets/swords.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'feder-compare';
+
+  public swordData: Array<Sword> = data;
+  public swords: Array<Sword> = [];
+
+  public ngOnInit(): void {
+    this.swords.push(this.swordData[0]);
+    this.swords.push(this.swordData[1]);
+    this.swords.push(this.swordData[4]);
+  }
+}
+
+interface Sword {
+  name: string;
+  handleLength: number;
+  bladeLength: number;
+  pob?: number;
+  totalWeight: number;
+  flex?: number;
+  price: number;
+  imagePrefix: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgSelectModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
