2e2ba84f939c18cbd9cdc15ab01f5ec5f76f66b3
Andrew Costenoble <accostenoble@gmail.com>
2024-02-11 18:11:44 -0500

    Added weight, price, and sorting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent implements OnInit {
   public swordData: Array<Sword> = data;
   public swords: Array<Sword> = [];
   public units = 'metric';
+  private curSort: {field: string, dir: string} = {field: '', dir: ''};
 
   public ngOnInit(): void {
     this.swords.push(this.swordData[0]);
@@ -19,12 +20,43 @@ export class AppComponent implements OnInit {
     this.swords.push(this.swordData[4]);
   }
 
+  public showAll() {
+    this.swords = [];
+    this.swordData.forEach((sword) => this.swords.push(sword));
+  }
+
   public getLength(valInCm: number): number {
     return this.units === 'metric' ? valInCm : valInCm * 0.393701;
   }
 
+  public getWeight(valInG: number): number {
+    return this.units === 'metric' ? valInG : valInG * 0.00220462;
+  }
+
+  public sort(sortBy: string) {
+    const key = sortBy as keyof Sword;
+    const newDir = this.curSort.field === sortBy && this.curSort.dir === 'asc' ? 'desc' : 'asc';
+    const sortConst = newDir === 'asc' ? 1 : -1;
+    this.swords.sort((a, b) => {
+      const aVal = a[key]!;
+      const bVal = b[key]!;
+
+      if (aVal === bVal) {
+        return 0;
+      }
+
+      return aVal > bVal ? 1 * sortConst : -1 * sortConst;
+    });
+
+    this.curSort = {field: sortBy, dir: newDir};
+  }
+
   get lengthUnit(): string {
     return this.units === 'metric' ? 'cm' : '"'
+  }
+
+  get weightUnit(): string {
+    return this.units === 'metric' ? 'g' : 'lbs';
   }
 }
 
